06fc37da7b2c9de57c122dd0b387b804e130e209
Simon Pirko <sipi7gm@gmail.com>
2026-03-16 23:56:14 +0300

    Initial commit

---
+++ src/main/java/by/tms/ecommerceprojectc38onl/configuration/WebConfiguration.java
@@ -0,0 +1,23 @@
+package by.tms.ecommerceprojectc38onl.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/pages/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
---
+++ src/main/java/by/tms/ecommerceprojectc38onl/controller/HelloController.java
@@ -0,0 +1,17 @@
+package by.tms.ecommerceprojectc38onl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "hello";
+    }
+}
