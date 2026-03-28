a5512dfc9535ec3307ad3d8a966efa1d4f439e41
Dasha2203 <kitkatlolipop@mail.ru>
2026-03-28 20:18:53 +0300

    feat: add Category DTO

---
+++ src/main/java/by/tms/ecommerceprojectc38onl/dto/CategoryDTO.java
@@ -0,0 +1,14 @@
+package by.tms.ecommerceprojectc38onl.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    @NotBlank
+    private String name;
+}
